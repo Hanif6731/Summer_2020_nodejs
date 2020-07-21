c0f08fe4b9a237c68edeacb2a76951e78a2fb929
Md. Hanif <mdhanif6731@gmail.com>
2020-07-21 11:24:11 +0600

    server create

---
+++ nodejs/student.js
@@ -0,0 +1,16 @@
+var std_id = "12345";
+var std_name = "Hanif";
+var std_dept="cse";
+
+var getName = function(){
+	return std_name;
+}
+
+
+module.exports
+
+
+module.exports.id=std_id;
+module.exports.name=std_name;
+module.exports.dept=std_dept;
+module.exports.getName=getName;
